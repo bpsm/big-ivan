25e2bb138bb05525f5a20e7b0783cec94a89a900
Ben Smith-Mannschott <bsmith.occs@gmail.com>
2012-06-21 20:41:02 +0200

    fill out project.clj

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject big-ivan "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "A validating parser for BIC and IBAN"
+  :url "http://github.com/bpsm/big-ivan"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.3.0"]])
+  :dependencies [[org.clojure/clojure "1.4.0"]])
